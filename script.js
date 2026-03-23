6da963c3e5cf140b0336d1ad65c365878cb40b14
GoldenGlint <darhgo@gmail.com>
2026-03-23 14:27:22 -0400

    Create redrawDiv Function
    now makes it so when you hit that button, it redraws the div

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ for(let i=0; i<16; i++){
     }
     container.appendChild(row);
 }
+
+let dimension=document.querySelector(".dimension")
+dimension.addEventListener("click", (item) => {
+  const el = item.target;
+
+  if (el.classList.contains("size")){
+    let num = Math.min(100, prompt("Give the size of one side: ", 16));
+    redrawDiv(num);
+  }
+  else if (el.classList.contains("clear")) console.log("clear");
+  else if (el.classList.contains("erase")) console.log("erase");
+
+});
+
+function redrawDiv(num){
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+    for(let i=0; i<num; i++){
+    let row=document.createElement("div");
+    
+    let ident="row-"+ i;
+    row.id=ident;
+    row.classList.add('row');
+
+    for(let l=0; l<num; l++){
+        let div=document.createElement("div");
+        div.classList.add("square");
+        div.style.backgroundColor="white";
+        row.appendChild(div);
+    }
+    container.appendChild(row);
+}
+
+};
